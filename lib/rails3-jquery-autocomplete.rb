4e2e5f91efb705fdf780c743ee4d0c59debf68b7
Artem Harmaty <AHarmaty@sphereconsultinginc.com>
2010-09-02 20:53:26 +0400

    Autocomplete displays now unique items

--- lib/rails3-jquery-autocomplete.rb
+++ lib/rails3-jquery-autocomplete.rb
@@ -30,7 +30,7 @@ module Rails3JQueryAutocomplete
 
       define_method("autocomplete_#{object}_#{method}") do
         unless params[:term] && params[:term].empty?
-          items = object.to_s.camelize.constantize.where(["LOWER(#{method}) LIKE ?", "#{(options[:full] ? '%' : '')}#{params[:term].downcase}%"]).limit(limit).order(order)
+          items = object.to_s.camelize.constantize.select("DISTINCT #{method}").where(["LOWER(#{method}) LIKE ?", "#{(options[:full] ? '%' : '')}#{params[:term].downcase}%"]).limit(limit).order(order)
         else
           items = {}
         end
